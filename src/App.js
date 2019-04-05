4a593bf2dcbb9234fb830f00f8a0dee675a6db46
Anisha Jain <anishajain@outlook.com>
2019-04-05 00:35:41 -0700

    Added initial message popup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import GridBoard from './components/grid-board'
 import NextBlock from './components/next-block'
 import ScoreBoard from './components/score-board'
 import Controls from './components/controls'
+import MessagePopup from './components/message-popup'
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
         <NextBlock />
         <ScoreBoard />
         <Controls />
+        <MessagePopup />
       </div>
     );
   }
